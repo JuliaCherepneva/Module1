5adb8f03d5a7faa6164b89b36156a955a9a66d2a
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-04-11 12:46:30 +0400

    Создала свою реализацию HashMap и протестировала её

--- src/main/java/com/gridnine/Main.java
+++ src/main/java/com/gridnine/Main.java
@@ -1,5 +1,16 @@
 package com.gridnine;
 
 public class Main {
+    public static void main(String[] args) {
+        MyHashMap<Integer, String> map = new MyHashMap<>();
 
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+
+        System.out.println(map);
+        System.out.println(map.get(3));
+        map.remove(3);
+        System.out.println(map.get(3));
+    }
 }
